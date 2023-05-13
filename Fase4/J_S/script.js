103287af50e92374a8f378113f4f862219096e08
CRISTIAN172000 <magicmac741@gmail.com>
2023-05-13 01:16:15 -0500

    Fase4_Cuestionario
    Se realizo un cuestionario con base a la tema seleccionada aplicando estilo css y programación de JavaScript junto con HTML, se personalizo la pagina al gusto para que contrastara con el diseño minimalista de la encuesta.

---
+++ Fase4/J_S/script.js
@@ -0,0 +1,22 @@
+function ProcesarRespuestas() {
+    let total = 3
+    let puntos = 0
+
+    let myForm = document.forms["formulario"];
+    let RespuestasCorrectas = ["c", "a", "b", "a", "b", "c"];
+
+    for (let i =1; i <= total; i++) {
+        if (myForm["r" + i].value == null ||
+        myForm["r" + i].value == "") {
+            alert("Por favor responder todas las preguntas");
+            return false;
+        } else {
+            if (myForm["r" + i].value === RespuestasCorrectas[i - 1])
+            puntos++;
+        }
+    }
+    let resultado = document.getElementById('resultado')
+    resultado.innerHTML='Obtuviste ' +puntos+' puntos de '+total+ ' posibles';
+    return false;
+    
+}
